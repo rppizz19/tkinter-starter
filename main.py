b4eca5a077dd5100526b5d04629b0b2a2e9f316a
Ronald Pizzini <rppizz19>
2020-01-30 11:16:44 -0500

    Update main.py

--- main.py
+++ main.py
@@ -4,8 +4,12 @@ from tkinter import * # import Tkinter library
 window = Tk()         # Create the application window
 
 # Add a label with the text "Hello"
-lbl = Label(window, text="Hello")
-
+firstLabel = Label (window, text = "you eat all my beans") # Create the label 
+firstLabel.grid (column=0, row=0)                   # Place on screen
+lbl = Label(window, text="")
+firstLabel = Label (window, text="Hello", font =("Arial Bold" , 70  ))
+                                    # create the label 
+                                    # Place on screen
 # Place the label in the window at 0, 0
 lbl.grid(column=0, row=0)
 
